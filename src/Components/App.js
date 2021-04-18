74509afb8b0aab095e72d4127c272546bd620950
hossam <hosamabdulmaged@gmail.com>
2021-04-18 12:08:02 +0200

    initailize reactApp From Scratch Using webpack and babel And react react-dom

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const App = () => {
+    return (
+        <div style={{ textAlign: 'center', color: 'pink' }}>
+            <h1>Webpack + babel +  React setup</h1>
+        </div>
+    );
+}
+
+export default App;
